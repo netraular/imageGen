5e1c2e532c485157b08fa4a29e99abbf5c01c0ff
netraular <netraular@email.com>
2024-10-14 17:37:34 +0000

    Añadido bootstrap y estilos en local

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,12 +51,13 @@
 @stop
 
 @push('js')
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+@vite(['resources/js/app.js'])
+
 @endpush
 
 @push('css')
+@vite(['resources/sass/app.scss'])
+
 <style>
 .main-sidebar {
     height: 100vh; /* Ocupa todo el alto de la ventana */
